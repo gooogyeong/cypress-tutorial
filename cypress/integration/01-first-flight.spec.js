12f632b282d7cd7a476a98f5a30b242d91b852de
Minkyung Lee <gooogyeong@gmail.com>
2022-08-28 13:07:04 +0900

    chore: wrote first test case

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,23 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter')
+  })
+
+  it('should have a form', () => {
+    cy.get('form').should('exist')
+  })
+
+  // it('should have a form', () => {
+  //   cy.get('formm').should('not.exist')
+  // })
+
+  it('should be able to take user input', () => {
+    cy.get('[data-test="new-item-input"]').type('Good attitude')
+  })
+
+  it('should have the words "Add Item"', () => {
+    cy.contains('Add Item')
+  })
+})
